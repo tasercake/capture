13aad17ff6bbf0be37f55230a56ebd78d6657c72
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2020-09-22 12:52:09 +0800

    Video Source Preview

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -8,7 +8,7 @@ export default function Home(): JSX.Element {
     <div className={styles.container} data-tid="container">
       <Link to={routes.HOME}><h3>Home</h3></Link>
       <Link to={routes.COUNTER}><h3>Counter</h3></Link>
-      <Link to={routes.CAPTURE}><h3>Select Sharing Source</h3></Link>
+      <Link to={routes.CAPTURE}><h3>Select Video Source</h3></Link>
       <Link to="/broken-link"><h3>Test Broken Link</h3></Link>
     </div>
   );
--- app/features/capture/Selector.tsx
+++ app/features/capture/Selector.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect, useRef} from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../../constants/routes.json';
 import { desktopCapturer } from 'electron';
 
 export default function Selector() {
-  desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
-    console.log(sources)
-  })
+  const [videoSources, setVideoSources] = useState([])
+
+  useEffect(() => {
+    const refreshVideoSources = () => {
+      desktopCapturer.getSources({ types: ['window', 'screen'] }).then(
+        async sources => {
+          // await new Promise(r => setTimeout(r, 5000))
+          sources = sources.sort((a, b) => {
+            let nameA = a.name.toLowerCase()
+            let nameB = b.name.toLowerCase()
+            if (nameA < nameB) return -1
+            if (nameA > nameB) return 1
+            return 0
+          })
+          setVideoSources(sources)
+        }
+      )
+    }
+    refreshVideoSources()
+    const id = setInterval(refreshVideoSources, 1000)
+    return () => {
+      clearInterval(id)
+    }
+  }, [])
+
   return (
-    <div data-tid="container">
+    <div>
       <Link to={routes.HOME}><h3>Home</h3></Link>
+      <ul>
+        {
+          videoSources.map(source => {
+            return (
+              <Fragment key={source.name}>
+                <li>{source.name}</li>
+                <img src={source.thumbnail.toDataURL()}/>
+              </Fragment>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
