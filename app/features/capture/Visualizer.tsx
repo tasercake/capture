c811b15429857f79aa43e243e84979bbf05e05df
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2020-12-01 01:28:09 +0800

    Removed unused code

---
+++ app/features/capture/Visualizer.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import {
+  MediaPreview,
+  deviceSourceTypes,
+  DeviceSourcePreview,
+} from './Previews';
+
+export default function Visualizer() {
+
+  return (
+    <Container fluid>
+      <Row>
+
+      </Row>
+    </Container>
+    )
+}
